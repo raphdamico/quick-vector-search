6ddd408a9bb1591ca11d50ac90fd4dde1a57a147
raphdamico <raph.damico@gmail.com>
2023-11-04 12:48:54 -0700

    Added time logging

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 // Import the necessary functions from transformers library
 import { env, pipeline, cos_sim } from "https://cdn.jsdelivr.net/npm/@xenova/transformers";
-env.allowLocalModels = false;
+env.allowLocalModels = true;
+env.allowRemoteModels = true;
 
 async function loadModel() {
     // Ensure that 'sentence-transformers/all-MiniLM-L6-v2' is a valid model path for the transformers library you are using
@@ -10,16 +11,32 @@ async function loadModel() {
 
 export async function compareSentences(search_sentence, sentences_to_compare) {
 
-    const sentences = [search_sentence, ...sentences_to_compare];
-    const model = await loadModel();
-    let output = await model(sentences, { pooling: 'mean', normalize: true });
-    output = output.tolist();
-    let similarities = []
-    for (let i=0; i<sentences_to_compare.length; i++) {
-      similarities[i] = {
-        sentence: sentences_to_compare[i],
-        score: cos_sim(output[0].flat(), output[i+1].flat())
-      }
+  // Load model
+  const t_start = new Date().getTime();
+  console.log(`Start`)
+  const sentences = [search_sentence, ...sentences_to_compare];
+  const model = await loadModel();
+  
+  // Calculate vectors
+  const t_modelLoaded = new Date().getTime();
+  console.log(`Model load time ${((t_modelLoaded - t_start) / 1000 ).toFixed(2)}s`)
+  let output = await model(sentences, { pooling: 'mean', normalize: true });  
+  output = output.tolist();
+  
+  // Calculate cosine similarity 
+  const t_calculatedVectors = new Date().getTime();
+  console.log(`Processing vectors time ${((t_calculatedVectors - t_modelLoaded)/1000).toFixed(2)}s`)
+  let similarities = []
+  for (let i=0; i<sentences_to_compare.length; i++) {
+    similarities[i] = {
+      sentence: sentences_to_compare[i],
+      score: cos_sim(output[0].flat(), output[i+1].flat())
     }
-    return similarities;
+  }
+  
+  // Return output
+  const t_calculatedCosSim = new Date().getTime();
+  console.log(`Cosine similarity ${((t_calculatedCosSim - t_calculatedVectors)/1000).toFixed(2)}s`)  
+  console.log(`Total time ${((t_calculatedCosSim - t_start)/1000).toFixed(2)}s`)  
+  return similarities;
 }
